f7ff4acd9c678ee800a4bafe95f26064eefd96d6
hagbazzinfo001 <151969171+hagbazzinfo001@users.noreply.github.com>
2024-11-23 22:58:24 +0100

    Add files via upload

---
+++ src/js/open.js
@@ -0,0 +1,71 @@
+function showSidebar(){
+    const sideBar=document.querySelector("#sideBar")
+    sideBar.style.display="flex"
+}
+
+function hideSidebar(){
+    const sideBar=document.querySelector("#sideBar")
+    sideBar.style.display="none"
+}
+
+// just testing but it correct
+// var userName= prompt("what is you name");
+// function dashBoard(){
+//     console.log(userName)
+// }
+//  another snipet for switch 
+// function moveCommand(direction) {
+//     var whatHappens;
+
+//     switch (direction) {
+//         case "forward":
+//             whatHappens = "you encounter a monster";
+//             break;
+//         case "back":
+//             whatHappens = "you arrived home";
+//             break;
+//         case "right":
+//             whatHappens = "you found a river";
+//             break;
+//         case "left":
+//             whatHappens = "you run into a troll";
+//             break;
+//         default:
+//             whatHappens = "please enter a valid direction";
+//             break;
+//     }
+
+//     return whatHappens; // This returns the value of whatHappens
+// }
+
+// Get input and display result
+// var direction = prompt("Specify your direction (forward, back, right, left):");
+// console.log(moveCommand(direction)); // Logs the returned value from moveCommand
+// var movement = prompt("Specify your direction (forward, back, right, left):");
+
+// function moveCommand(movement) {
+//     var whatHappens;
+
+//     switch (movement) {
+//         case "forward":
+//             whatHappens = "you encounter a monster";
+//             break;
+//         case "back":
+//             whatHappens = "you arrived home";
+//             break;
+//         case "right":
+//             whatHappens = "you found a river";
+//             break;
+//         case "left":
+//             whatHappens = "you run into a troll";
+//             break;
+//         default:
+//             whatHappens = "please enter a valid direction";
+//             break;
+//     }
+
+//     alert(whatHappens);  
+//     return whatHappens;
+// }
+
+// moveCommand(movement);
